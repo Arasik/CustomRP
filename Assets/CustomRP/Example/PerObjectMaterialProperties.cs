638e9ac3627e929e0855457228e38c57bacb1d91
KisaraShiki <arasik@qq.com>
2023-08-02 09:35:01 +0800

    逐对象裁切

--- Assets/CustomRP/Example/PerObjectMaterialProperties.cs
+++ Assets/CustomRP/Example/PerObjectMaterialProperties.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 public class PerObjectMaterialProperties : MonoBehaviour {
 	
     static int baseColorId = Shader.PropertyToID("_BaseColor");
+    static int cutoffId = Shader.PropertyToID("_Cutoff");
     static MaterialPropertyBlock block;
     [SerializeField]
     Color baseColor = Color.white;
+    
+    [SerializeField, Range(0f, 1f)]
+    float cutoff = 0.5f;
+
     void Awake () {
 	    OnValidate();
     }
@@ -15,6 +20,7 @@ public class PerObjectMaterialProperties : MonoBehaviour {
 		    block = new MaterialPropertyBlock();
 	    }
 	    block.SetColor(baseColorId, baseColor);
+	    block.SetFloat(cutoffId, cutoff);
 	    GetComponent<Renderer>().SetPropertyBlock(block);
     }
 }
